eff457e2380d9f33327a7acd97a7a68c437e9781
mariamodat <mariamodat0@gmail.com>
2021-03-29 14:53:18 +0300

    done

--- js/buss.js
+++ js/buss.js
@@ -5,6 +5,8 @@ let secondIndex ;
 let thirdIndex ;
 let trials = 0;
 let rnd=[];
+let viewsArr=[];
+let votesArr=[]; 
 
 function BussMall(name) {
 
@@ -40,9 +42,10 @@ for (let i = 0 ; i< names.length ; i ++ )
 {
   new BussMall (names[i]);
 }
-console.log(BussMall.all);
+// console.log(BussMall.all);
 
-// console.table(BussMall.all);
+console.table(BussMall.all);
+
 let section = document.getElementById('img-sec');
 let firstImage = document.getElementById('first-img');
 let middle = document.getElementById('midd-img');
@@ -51,7 +54,7 @@ let third=document.getElementById('third-img');
 function render(){
 
   firstIndex=getRandomIntInclusive(0,BussMall.all.length -1);
-  console.log(firstIndex);
+  // console.log(firstIndex);
 
   firstImage.src= BussMall.all[firstIndex].imgPath;
   firstImage.title= BussMall.all[firstIndex].name;
@@ -96,6 +99,9 @@ function render(){
       thirdIndex-=1 ;
     }
   }
+}
+
+
 
 
 
@@ -105,6 +111,11 @@ function render(){
   {
 
     // e.preventDefault();
+    BussMall.all[firstIndex].views++ ;
+    BussMall.all[secondIndex].views++ ;
+    BussMall.all[thirdIndex].views++ ;
+
+
 
     if (e.target.id !== 'img-secs')
     {
@@ -114,7 +125,6 @@ function render(){
 
         BussMall.all[firstIndex].votes++ ;
         trials++;
-        BussMall.all[firstIndex].views++ ;
 
       }
 
@@ -122,26 +132,29 @@ function render(){
       {
         BussMall.all[secondIndex].votes++;
         trials++;
-        BussMall.all[secondIndex].views++ ;
 
       }
 
       else {
         BussMall.all[thirdIndex].votes++;
         trials++;
-        BussMall.all[thirdIndex].views++ ;
 
       }
 
 
     }
+    if (trials <5 )
+{
+render();
 
 
+}
 
-    if (trials===25)
-    {
-      alert('you have finished the trials');
-    }
+
+  }
+
+  render();
+
 
 
 let btn = document.getElementById('res');
@@ -153,37 +166,59 @@ function resultChart (event)
     // console.table(BussMall.all);
 
     let div = document.querySelector('.result');
-    let heading = document.createElement('h1');
-    div.append(heading);
-    heading.textContent=('the results of voting gonna be ');
-    let results=document.createElement('h2');
-    heading.append(results);
-    results.textContent= (` the photo that has highest voting reached ${BussMall.all.name} and  ${BussMall.all.votes} Votes ${BussMall.all.views} Views as well !`);
+    let ulElem = document.createElement('ul');
+    div.append(ulElem);
+    // ulElem.textContent=('the results of voting gonna be ');
+    for (let j=0 ; j<BussMall.all.length ; j++)
+    {
 
+    let liElem =document.createElement('li');
+    ulElem.append(liElem);
+    liElem.textContent= (` ${BussMall.all[j].name} it has  ${BussMall.all[j].votes} Votes and ${BussMall.all[j].views} Views as well !`);
+    }
 
+     for (let i = 0; i < BussMall.all.length; i++) {
+        votesArr.push(BussMall.all[i].votes);
+        viewsArr.push(BussMall.all[i].views);
+
+    // btn.removeEventListener('click', resultChart);
   }
-
-
-
-
-
-
-
-
-
-
-
-
-  }
-
-
-
-
 }
+
 
 render();
 
+// console.log(names);
+// console.log(votesArr);
+// console.log(viewsArr);
 
 
+function chartRender() {
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
 
+    // The data for our dataset
+    data: {
+      labels: names,
+      datasets: [{
+        label: 'Items Votes',
+        backgroundColor: '#003049',
+        borderColor: '#bc4b51',
+        data: votesArr
+      },
+      {
+        label: 'Items Views',
+        backgroundColor: '#f95738',
+        borderColor: '#aec3b0',
+        data: viewsArr
+      }]
+    },
 
+    // Configuration options go here
+    options: {}
+  });
+}
+
+chartRender();
